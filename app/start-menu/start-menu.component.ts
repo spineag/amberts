a30acf96ec956e7d372c88f136fcd2bc5d864242
spineag <spineag@gmail.com>
2020-04-27 21:42:42 +0300

    1 file modified

--- app/start-menu/start-menu.component.ts
+++ app/start-menu/start-menu.component.ts
@@ -12,7 +12,7 @@ export class StartMenuComponent implements OnInit {
   constructor(private iconRegistry: MatIconRegistry, private sanitizer: DomSanitizer) { 
     iconRegistry.addSvgIcon(
         "amberts-logo",
-        sanitizer.bypassSecurityTrustResourceUrl('assets/svg/logo/amberts-logo.svg'));
+        sanitizer.bypassSecurityTrustResourceUrl('https://raw.githubusercontent.com/spineag/amberts/6ac5e6f09286cf4bcd75ddfab0654a0b18dbf1ed/assets/svg/logo/amberts-logo.svg'));
         // sanitizer.bypassSecurityTrustResourceUrl('https://upload.wikimedia.org/wikipedia/commons/2/21/Speaker_Icon.svg'));
   }
 

8c2a44041015da039f20689c91137c9436cbb629
spineag <spineag@gmail.com>
2020-05-26 22:08:44 +0300

    1 file modified

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,24 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { MatIconModule } from '@angular/material/icon';
 
-import { routing } from './app.routing';
-
 import { UserService } from './shared/services/user.service';
 
+import { routing } from './app.routing';
 import { AppComponent } from './app.component';
 import { StartComponent } from './start-menu/start.component';
 import { StartMenuComponent } from './start-menu/start-menu.component';
-import { UserListComponent } from './temp/user-list/user-list.component';
-import { UserCreateComponent } from './temp/user-create/user-create.component';
-import { UserSingleComponent } from './temp/user-single/user-single.component';
-import { UserEditComponent } from './temp/user-edit/user-edit.component';
+import { PageListEventsComponent } from './page-list-events/page-list-events.component';
 import { StartMenuItemComponent } from './start-menu/start-item-menu/start-menu-item.component';
 
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/throw';
-
-
 
 
 @NgModule({
@@ -30,8 +24,7 @@ import 'rxjs/add/observable/throw';
     routing, MatIconModule
   ],
   declarations: [
-    AppComponent,StartComponent,UserListComponent,UserCreateComponent,UserSingleComponent,
-    UserEditComponent,StartMenuComponent,StartMenuItemComponent],
+    AppComponent,StartComponent,StartMenuComponent,StartMenuItemComponent,PageListEventsComponent],
   providers: [
     UserService
   ],

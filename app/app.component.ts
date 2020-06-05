a309697bb992e3c89751e96b79bd5a3145621732
spineag <spineag@gmail.com>
2020-06-05 23:51:29 +0300

    8 files modified

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,7 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
+    <div class="outer_main_container">
+    <div class="outer_main_undercontainer">
     <router-outlet></router-outlet>
+    </div>
+    </div>
   `,
   styles: [`
 

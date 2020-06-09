75599ebe0e0a262ef69303f70907246b837984b4
spineag <spineag@gmail.com>
2020-06-09 22:15:23 +0300

    1 file modified, 1 added

--- app/shared/models/action.ts
+++ app/shared/models/action.ts
@@ -1,6 +1,17 @@
 export class Action {
-  id?: number;
-  name: string;
-  username: string;
-  avatar: string
+  id: Number;
+  name: String;
+  short_description: String;
+  description: String;
+  type: Number;
+  dateStart: Date;
+  dateEnd: Date;
+  route: String;
+
+  speedRange: String; // 5:30 - 6:00 hv/km OR 20-25 km/hour
+  hard: Number; // 1-5 - no any need, it can be automatic later
+  routeLength: String; // 10 km OR 50 - 55 km
+
+  ownerUserId: String;
+  ownerClubId: String;
 }
---
+++ app/shared/static/objects.ts
@@ -0,0 +1,8 @@
+export let ActionType={
+  BIKE:{id:1, nameUA:'Велосипед', icon:'bike'},
+  RUN:{id:2, nameUA:'Біг', icon:'run'},
+  SWIM:{id:3, nameUA:'Плавання', icon:'swim'},
+  WALK:{id:4, nameUA:'Похід', icon:'walk'},
+  CAMP:{id:5, nameUA:'Відпочинок', icon:'camp'},
+  CAFE:{id:6, nameUA:'Кафе', icon:'coffee'}
+};

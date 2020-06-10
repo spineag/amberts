f5bbe484680ef9e1be45f7d1e9ba342484fd60af
spineag <spineag@gmail.com>
2020-06-10 18:47:52 +0300

    4 files modified

--- app/shared/static/objects.ts
+++ app/shared/static/objects.ts
@@ -1,8 +1,9 @@
-export let ActionType={
-  BIKE:{id:1, nameUA:'Велосипед', icon:'bike'},
-  RUN:{id:2, nameUA:'Біг', icon:'run'},
-  SWIM:{id:3, nameUA:'Плавання', icon:'swim'},
-  WALK:{id:4, nameUA:'Похід', icon:'walk'},
-  CAMP:{id:5, nameUA:'Відпочинок', icon:'camp'},
-  CAFE:{id:6, nameUA:'Кафе', icon:'coffee'}
-};
+export let actionTypes=[
+  {id:1, type:'BIKE', nameUA:'Велосипед', icon:'directions_bike', typeIcon:'material'},
+  {id:2, type:'RUN', nameUA:'Біг', icon:'directions_run', typeIcon:'material'},
+  {id:3, type:'SWIM', nameUA:'Плавання', icon:'pool', typeIcon:'material'},
+  {id:4, type:'WALK', nameUA:'Похід', icon:'directions_walk', typeIcon:'material'},
+  {id:5, type:'REST', nameUA:'Відпочинок', icon:'outdoor_grill', typeIcon:'material'},
+  {id:6, type:'CAFE', nameUA:'Кафе', icon:'local_cafe', typeIcon:'material'},
+  {id:7, type:'OTHER', nameUA:'Інше', icon:'category', typeIcon:'material'}
+];

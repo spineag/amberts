09bb20be5c7a2861310c78d0c86ecdca23b7c66f
spineag <spineag@gmail.com>
2020-06-22 23:05:56 +0300

    3 files modified

--- app/page-create-event/page-create-event.component.ts
+++ app/page-create-event/page-create-event.component.ts
@@ -12,8 +12,21 @@ export class PageCreateEventComponent implements OnInit {
   ngOnInit() {
   }
 
-  onPressKey_Name(event:any):void{
+  onPressKey(event:any):void{
+    let el:HTMLInputElement = event.target, counter:HTMLSpanElement = document.querySelector('#'+el.name+'_counter');
+    counter.innerText=el.value.length+'/'+el.maxLength;
+  }
 
+  onFocus(event:any):void{
+    let el:HTMLInputElement = event.target, counter:HTMLSpanElement = document.querySelector('#'+el.name+'_counter');
+    counter.hidden=false;
+    if (el.name=='event_long') el.style.height='30vh';
+  }
+
+  onFocusOut(event:any):void{
+    let el:HTMLInputElement = event.target, counter:HTMLSpanElement = document.querySelector('#'+el.name+'_counter');
+    counter.hidden=true;
+    if (el.name=='event_long') el.style.height='5vh';
   }
 
 }
